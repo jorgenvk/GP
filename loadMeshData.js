a0a59f786a51f7dbc2bdcb56ae14b3eda08f0151
Jørgen <karjor16@student.westerdals.no>
2018-10-18 14:06:53 +0200

    hehe

---
+++ loadMeshData.js
@@ -0,0 +1,10 @@
+function loadMeshData(string){
+    var lines = string.split("\n");
+    var positions = [];
+    var normals = [];
+    var vertices = [];
+
+    for( var i = 0; i < lines.length; i++){
+        var parts = lines[i].trimRight().split(' ');
+    }
+}
--- wgl.js
+++ wgl.js
@@ -12,16 +12,28 @@ function main(){
 // Vertex Shader
     const vsSource = `
     attribute vec4 aVertexPosition;
+    attribute vec3 aVertexNormal;
     attribute vec2 aTextureCoord;
     
+    uniform mat4 uNormalMatrix;
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
     
     varying highp vec2 vTextureCoord;
+    varying highp vec3 vLighting;
     
     void main(){
     gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
     vTextureCoord = aTextureCoord;
+    
+    highp vec3 ambientLight = vec3(.3, 0.3, 0.3);
+    highp vec3 directionalLightColor = vec3(1, .5, .5);
+    highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
+    
+    highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
+    
+    highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
+    vLighting = ambientLight + (directionalLightColor * directional);
     }
 `;
 
@@ -29,11 +41,14 @@ function main(){
 // Fragment Shader
     const fsSource = `
         varying highp vec2 vTextureCoord;
+        varying highp vec3 vLighting;
         
         uniform sampler2D uSampler;
         
         void  main(){
-            gl_FragColor = texture2D(uSampler, vTextureCoord);
+            highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
+        
+            gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
          }
 `;
 
@@ -43,11 +58,13 @@ function main(){
         program: shaderProgram,
         attribLocations: {
             vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+            vertexNormal: gl.getAttribLocation(shaderProgram, "aVertexNormal"),
             textureCoord: gl.getAttribLocation(shaderProgram, "aTextureCoord"),
         },
         uniformLocations: {
             projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
             modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+            normalMatrix: gl.getUniformLocation(shaderProgram, "uNormalMatrix"),
             uSampler: gl.getUniformLocation(shaderProgram, "uSampler"),
         },
     };
@@ -71,113 +88,6 @@ function main(){
 
 }
 
-function initBuffers(gl){
-
-    const vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-
-    const vertices = [
-        // Front face
-        -1.0, -1.0,  1.0,
-        1.0, -1.0,  1.0,
-        1.0,  1.0,  1.0,
-        -1.0,  1.0,  1.0,
-
-        // Back face
-        -1.0, -1.0, -1.0,
-        -1.0,  1.0, -1.0,
-        1.0,  1.0, -1.0,
-        1.0, -1.0, -1.0,
-
-        // Top face
-        -1.0,  1.0, -1.0,
-        -1.0,  1.0,  1.0,
-        1.0,  1.0,  1.0,
-        1.0,  1.0, -1.0,
-
-        // Bottom face
-        -1.0, -1.0, -1.0,
-        1.0, -1.0, -1.0,
-        1.0, -1.0,  1.0,
-        -1.0, -1.0,  1.0,
-
-        // Right face
-        1.0, -1.0, -1.0,
-        1.0,  1.0, -1.0,
-        1.0,  1.0,  1.0,
-        1.0, -1.0,  1.0,
-
-        // Left face
-        -1.0, -1.0, -1.0,
-        -1.0, -1.0,  1.0,
-        -1.0,  1.0,  1.0,
-        -1.0,  1.0, -1.0,
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-
-    const textureCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
-
-    const textureCoordinates = [
-        // Front
-        0.0,  0.0,
-        1.0,  0.0,
-        1.0,  1.0,
-        0.0,  1.0,
-        // Back
-        0.0,  0.0,
-        1.0,  0.0,
-        1.0,  1.0,
-        0.0,  1.0,
-        // Top
-        0.0,  0.0,
-        1.0,  0.0,
-        1.0,  1.0,
-        0.0,  1.0,
-        // Bottom
-        0.0,  0.0,
-        1.0,  0.0,
-        1.0,  1.0,
-        0.0,  1.0,
-        // Right
-        0.0,  0.0,
-        1.0,  0.0,
-        1.0,  1.0,
-        0.0,  1.0,
-        // Left
-        0.0,  0.0,
-        1.0,  0.0,
-        1.0,  1.0,
-        0.0,  1.0,
-    ];
-
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates),
-        gl.STATIC_DRAW);
-
-
-    const indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-    const indices = [
-        0,  1,  2,      0,  2,  3,    // front
-        4,  5,  6,      4,  6,  7,    // back
-        8,  9,  10,     8,  10, 11,   // top
-        12, 13, 14,     12, 14, 15,   // bottom
-        16, 17, 18,     16, 18, 19,   // right
-        20, 21, 22,     20, 22, 23,   // left
-    ];
-
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
-        new Uint16Array(indices), gl.STATIC_DRAW);
-
-    return{
-        position: vertexBuffer,
-        textureCoord: textureCoordBuffer,
-        indices: indexBuffer,
-    };
-
-}
 
 function initShaderProgram(gl, vsSource, fsSource){
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
@@ -208,106 +118,6 @@ function loadShader(gl, type, source){
         return null;
     }
     return shader;
-}
-
-function drawScene(gl, programInfo, buffers, texture, deltaTime){
-    gl.clearColor(0.5, 0.5, 0.5, 1);
-    gl.clearDepth(1.0);
-    gl.enable(gl.DEPTH_TEST);
-    gl.depthFunc(gl.LEQUAL);
-
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    const fOV = 45 * Math.PI / 180;
-    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    const zNear = 0.1;
-    const zFar = 100.0;
-    const projectionMatrix = mat4.create();
-
-    mat4.perspective(
-        projectionMatrix,
-        fOV,
-        aspect,
-        zNear,
-        zFar);
-
-    const modelViewMatrix = mat4.create();
-
-    mat4.translate(
-        modelViewMatrix,
-        modelViewMatrix,
-        [-0.0, 0.0, -6.0]);
-
-    mat4.rotate(
-        modelViewMatrix,
-        modelViewMatrix,
-        cubeRotation,
-        [0, 0, 1]);
-
-    mat4.rotate(
-        modelViewMatrix,
-        modelViewMatrix,
-        cubeRotation * 0.7,
-        [0,1,0]
-    );
-
-    {
-        const numComponents = 3;
-        const type = gl.FLOAT;
-        const normalize = false;
-        const stride = 0;
-        const offset = 0;
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-        gl.vertexAttribPointer(
-            programInfo.attribLocations.vertexPosition,
-            numComponents,
-            type,
-            normalize,
-            stride,
-            offset);
-        gl.enableVertexAttribArray(
-            programInfo.attribLocations.vertexPosition);
-    }
-
-    {
-        const num = 2;
-        const type = gl.FLOAT;
-        const normalize = false;
-        const stride = 0;
-        const offset = 0;
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
-        gl.vertexAttribPointer(programInfo.attribLocations.textureCoord,
-            num, type, normalize, stride, offset);
-        gl.enableVertexAttribArray(programInfo.attribLocations.textureCoord);
-
-
-    }
-
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
-
-    gl.useProgram(programInfo.program);
-
-    gl.uniformMatrix4fv(
-        programInfo.uniformLocations.projectionMatrix,
-        false,
-        projectionMatrix);
-
-    gl.uniformMatrix4fv(
-        programInfo.uniformLocations.modelViewMatrix,
-        false,
-        modelViewMatrix);
-
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
-
-    {
-        const vertexCount = 36;
-        const type = gl.UNSIGNED_SHORT;
-        const offset = 0;
-        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
-    }
-    cubeRotation += deltaTime;
 }
 
 function loadTexture(gl, url){
